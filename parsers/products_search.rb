8fd3dc33dc17bc9381e6c125cbf9abde85fc8132
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-08 13:28:40 +0100

    Update scraper.

--- parsers/products_search.rb
+++ parsers/products_search.rb
@@ -143,7 +143,7 @@ products.each_with_index do |product, i|
   pages << {
       page_type: 'product_reviews',
       method: 'GET',
-      url: "https://www.mat.se/api/v1.2/product/#{product['id']}/review/list",
+      url: "https://www.mat.se/api/v1.2/product/#{product['id']}/review/list?&searchkeyword=#{page['vars']['search_term']}&searchpage=#{page['vars']['page']}",
       vars: {
           'product_details' => product_details
       }
